01dd4b997a1c1a150a7f6d547b44be3d5d3dba6b
Austris Landmanis <aus3ys@gmail.com>
2014-03-19 16:40:57 +0200

    Init... Bird vs. The Mechanic!

---
+++ Assets/button.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+// JavaScript
+function OnGUI () {
+	if (GUI.Button (Rect (10,100,100,30), "KILL BIRD!")) {
+		print ("Die BIRD!!!");
+	}
+}
---
+++ Assets/d.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var scorePts: int = 0;
+
+function Start () {
+
+}
+
+function Update () {
+	guiText.text = "<color=gray>SCORE</color>: <size=50>"+ scorePts.ToString() +"</size>";
+}
+
+function OnGUI () {
+	if (GUI.Button (Rect (10,100,100,30), "KILL BIRD!")) {
+		scorePts++;
+	}
+}
